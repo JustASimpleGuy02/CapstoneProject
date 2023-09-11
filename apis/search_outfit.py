d86086e7a1fe0f90016d5d9afb96533045b14ab9
dungnq271 <dungnguyen2712002@gmail.com>
2023-09-11 14:11:08 +0700

    Add function to load hashnet model for outfit recommendation

--- apis/search_outfit.py
+++ apis/search_outfit.py
@@ -1,5 +1,29 @@
-# from .search_item import search
-from Hash4AllFashion.train import get_net
+import sys
+sys.path += ["Hash4AllFashion"]
+from search_item import search
+
+def get_net(config, logger):
+    """Get network."""
+    # Get net param
+    net_param = config.net_param
+    logger.info(f"Initializing {utils.colour(config.net_param.name)}")
+    logger.info(net_param)
+
+    assert config.load_trained is not None
+
+    # Dimension of latent codes
+    net = FashionNet(net_param, logger, config.train_data_param.cate_selection)
+    # Load model from pre-trained file
+    num_devices = torch.cuda.device_count()
+    map_location = {"cuda:{}".format(i): "cpu" for i in range(num_devices)}
+    logger.info(f"Loading pre-trained model from {config.load_trained}")
+    state_dict = torch.load(config.load_trained, map_location=map_location)
+    # load pre-trained model
+    net.load_state_dict(state_dict)
+    logger.info(f"Copying net to GPU-{config.gpus[0]}")
+    net.cuda(device=config.gpus[0])
+    net.eval()  # Unable training
+    return net
 
 if __name__ == "__main__":
     print("oke")
