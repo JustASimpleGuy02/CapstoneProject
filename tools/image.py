df98e6e4544c7c0d6facfc748067db14f01e3dee
dungnq271 <dungnguyen2712002@gmail.com>
2023-09-27 01:07:15 +0700

    Add base64_to_image function

--- tools/image.py
+++ tools/image.py
@@ -1,5 +1,7 @@
 import random
 from typing import Union, Tuple, List
+import base64
+from io import BytesIO
 
 import numpy as np
 from PIL import Image
@@ -39,6 +41,16 @@ def load_image(
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     elif backend == "pillow":
         image = np.array(Image.open(path_to_image))
+
+    return image
+
+
+def base64_to_image(base64_string):
+    # Decode the Base64 string to bytes
+    image_bytes = base64.b64decode(base64_string)
+
+    # Create an Image object from the bytes
+    image = Image.open(BytesIO(image_bytes))
 
     return image
 
