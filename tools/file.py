856a29c0d9c3a723dedd5a20a240eefea64acc54
dungnq271 <dungnguyen2712002@gmail.com>
2023-09-27 01:06:17 +0700

    Add verbose param in load_json function

--- tools/file.py
+++ tools/file.py
@@ -3,7 +3,7 @@ from pprint import pprint
 from typing import Dict, List, Union
 
 
-def load_json(path: str) -> Union[Dict, List[Dict]]:
+def load_json(path: str, verbose: bool = False) -> Union[Dict, List[Dict]]:
     """Load a json file from path
 
     Args:
@@ -14,18 +14,19 @@ def load_json(path: str) -> Union[Dict, List[Dict]]:
     """
     data = json.load(open(path, "r", encoding="UTF-8"))
 
-    print("Number of key-value pairs:", len(data))
-    print("Type of data:", type(data))
+    if verbose:
+        print("Number of key-value pairs:", len(data))
+        print("Type of data:", type(data))
 
-    if isinstance(data, dict):
-        first_pair = next(iter(data.items()))
-        print(f"First Pair:")
-        key, val = first_pair
-        print("Key:", key)
-        print("Value:")
-        pprint(val)
-    elif isinstance(data, list):
-        print("First element:")
-        pprint(data[0])
+        if isinstance(data, dict):
+            first_pair = next(iter(data.items()))
+            print(f"First Pair:")
+            key, val = first_pair
+            print("Key:", key)
+            print("Value:")
+            pprint(val)
+        elif isinstance(data, list):
+            print("First element:")
+            pprint(data[0])
 
     return data
