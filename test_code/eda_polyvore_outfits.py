1fb90ceff4882326eba20a28f43c62d72c6685bd
dungnq271 <dungnguyen2712002@gmail.com>
2023-09-29 00:54:45 +0700

    Test subcategories' frequency for categories bottom and shoe

--- test_code/eda_polyvore_outfits.py
+++ test_code/eda_polyvore_outfits.py
@@ -3,21 +3,29 @@ import os
 import os.path as osp
 import sys
 
-sys.path.append("../")
 import random
 from pprint import pprint
+
+from tqdm import tqdm
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sys.path.append("../")
 from tools import *
 
 # %%
-data_dir = "../data/polyvore"
+data_dir = "/home/dungmaster/Datasets/polyvore_outfits"
 image_dir = osp.join(data_dir, "images")
 
 # %%
-outfit_titles = load_json(osp.join(data_dir, "polyvore_outfit_titles.json"))
+outfit_titles = load_json(osp.join(data_dir, "polyvore_outfit_titles.json"), verbose=True)
+
+# %%
+item_metadata = load_json(osp.join(data_dir, "polyvore_item_metadata.json"), verbose=True)
 
 # %%
 train_disjoin_outfit_items = load_json(
-    osp.join(data_dir, "disjoint", "train.json")
+    osp.join(data_dir, "disjoint", "train.json"), verbose=True
 )
 valid_disjoin_outfit_items = load_json(
     osp.join(data_dir, "disjoint", "valid.json")
@@ -45,6 +53,58 @@ test_nondisjoin_outfit_items = load_json(
 )
 
 # %%
+doubt_cates = ["bottoms", "shoes"]
+id2cates = pd.read_csv(osp.join(data_dir, "categories.csv"),
+                       index_col=None,
+                       names=["id", "category", "semantic_category"])
+id2cates.head()
+shoeid = list(set(id2cates[id2cates["semantic_category"] == "shoes"]["id"]))
+bottomid = list(set(id2cates[id2cates["semantic_category"] == "bottoms"]["id"]))
+shoeid
+
+# %%
+shoe_freqs = {}
+bottom_freqs = {}
+chosen_ids = shoeid + bottomid
+for meta in tqdm(item_metadata.values()):
+    cate_id = int(meta["category_id"])
+    if cate_id not in chosen_ids:
+        continue
+    cate = id2cates[id2cates["id"] == cate_id]["category"].iloc[0]
+    if cate_id in shoeid:
+        shoe_freqs[cate] = shoe_freqs.get(cate, 0) + 1
+    else:
+        bottom_freqs[cate] = bottom_freqs.get(cate, 0) + 1
+
+shoe_freqs, bottom_freqs
+
+# %%
+shoe_kv = {"category": list(shoe_freqs.keys()),
+           "frequency": list(shoe_freqs.values())
+           }
+shoe_df = pd.DataFrame(shoe_kv)
+shoe_df = shoe_df.sort_values("frequency", ascending=False).head(5)
+
+my_plot = sns.barplot(data=shoe_df, x="category", y="frequency")
+plt.title("Top shoe categories frequency")
+my_plot.set_xticklabels(my_plot.get_xticklabels())
+plt.savefig("../save_figs/shoe_categories_frequency.png")
+
+# %%
+bottom_kv = {"category": list(bottom_freqs.keys()),
+             "frequency": list(bottom_freqs.values())
+             }
+bottom_kv
+
+bottom_df = pd.DataFrame(bottom_kv)
+bottom_df = bottom_df.sort_values("frequency", ascending=False).head(5)
+
+my_plot = sns.barplot(data=bottom_df, x="category", y="frequency")
+plt.title("Top bottom categories frequency")
+my_plot.set_xticklabels(my_plot.get_xticklabels())
+plt.savefig("../save_figs/bottom_categories_frequency.png")
+
+# %%
 idx = random.randint(0, len(train_disjoin_outfit_items) - 1)
 set_id = train_disjoin_outfit_items[idx]["set_id"]
 set_items = train_disjoin_outfit_items[idx]["items"]
@@ -56,7 +116,7 @@ pprint(set_items)
 # %%
 outfit_data = train_disjoin_outfit_items
 n_outfits = len(outfit_data)
-n_sample = 10
+n_sample = 5
 rand_inds = random.sample(range(0, n_outfits - 1), n_sample)
 
 sample_images = []
