ecec4128e15b802968b3e92559acace62e8e943d
dungnq271 <dungnguyen2712002@gmail.com>
2023-11-01 23:40:22 +0700

    Add frequency display of some of the values of attributes in theme-aware-dataset

--- test_code/eda_theme-aware-dataset.py
+++ test_code/eda_theme-aware-dataset.py
@@ -6,6 +6,7 @@ import sys
 import random
 from pprint import pprint
 import importlib
+import json
 
 # from functools import map
 
@@ -14,18 +15,58 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from PIL import Image
 
-sys.path.append("../")
-
-from tools import image, load_image, load_json, display_image_sets
-
-importlib.reload(image)
+from translatepy.translators.google import GoogleTranslateV2
+from reproducible_code.tools import load_json, display_image_sets, plot_attribute_frequency, load_pickle
 
 sns.set_theme()
+tqdm.pandas()
 
 # %% Loading data
-data_dir = "/home/dungmaster/Datasets/Fashion_Outfits_Theme_Aware"
+data_dir = "/home/dungmaster/Datasets/Fashion-Outfits-Theme-Aware"
 outfits_dir = osp.join(data_dir, "outfits")
+
+# %% Load csv
+df_outfit_meta = pd.read_csv(
+    osp.join(data_dir, "outfit_meta.csv"), index_col=None
+)
+df_outfit_meta.head(5)
+
+# %%
+df_outfit_meta.isna().sum()
+
+# %%
+df = df_outfit_meta["en_Outfit_Description"].dropna().apply(lambda x: len(x)).rename("description length")
+sns.histplot(df)
+
+# %%
+df = df_outfit_meta["en_Outfit_Name"].dropna().apply(lambda x: len(x)).rename("name length")
+sns.histplot(df)
+
+# %%
+field = "en_Outfit_Style"
+df = df_outfit_meta[field].dropna().value_counts().reset_index()
+ax = sns.barplot(df, x=field, y="index")
+ax.bar_label(ax.containers[0], fontsize=10);
+
+# %%
+field = "en_Outfit_Occasion"
+df = df_outfit_meta[field].dropna().value_counts().reset_index()
+ax = sns.barplot(df, x=field, y="index")
+ax.bar_label(ax.containers[0], fontsize=10);
+
+# %%
+field = "en_Outfit_Fit"
+df = df_outfit_meta[field].dropna().value_counts().reset_index()
+ax = sns.barplot(df, x=field, y="index")
+ax.bar_label(ax.containers[0], fontsize=10);
+
+# %%
+field = "en_Outfit_Gender"
+df = df_outfit_meta[field].dropna().value_counts().reset_index()
+ax = sns.barplot(df, x=field, y="index")
+ax.bar_label(ax.containers[0], fontsize=10);
 
 # %%
 outfit_descriptions = load_json(
@@ -33,13 +74,14 @@ outfit_descriptions = load_json(
 )
 outfit_ids = list(outfit_descriptions.keys())
 
-# %%
+# %% Display outfits with their descriptions
 n_outfits = len(outfit_ids)
 n_sample = 1
 rand_inds = random.sample(range(0, n_outfits - 1), n_sample)
 
 sample_images = []
 sample_outfit_titles = []
+image_2ds = []
 
 for ind in rand_inds:
     outfit_id = outfit_ids[ind]
@@ -59,15 +101,17 @@ for ind in rand_inds:
 
     for item_info in outfit_meta["Items"]:
         image_path = osp.join(outfit_dir, item_info["Image"])
-        image = np.array(load_image(image_path))
+        image = np.array(Image.open(image_path))
         if len(image.shape) < 3:
+            print("2d image")
+            image_2ds.append(image)
             image = image[..., np.newaxis].repeat(3, -1)
         item_images.append(image)
     item_images = np.hstack(item_images)
 
     for outfit_image in outfit_meta["Outfit_Images"]:
         outfit_image_path = osp.join(outfit_dir, outfit_image)
-        outfit_image = np.array(load_image(outfit_image_path))
+        outfit_image = np.array(Image.open(outfit_image_path))
         outfit_images.append(outfit_image)
     outfit_images = np.hstack(outfit_images)
 
@@ -81,6 +125,5 @@ for ind in rand_inds:
 display_image_sets(sample_images, title=outfit_text)
 
 # %%
-outfit_id
+plt.imshow(np.hstack(image_2ds), cmap="gray")
 
-# %%
